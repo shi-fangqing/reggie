b90f6f8e8a1edd11ddcf34a8ba97f872b3f8f7f7
wuyu <wuyu@git.com>
2023-04-11 15:21:23 +0800

    master add

--- src/main/java/com/shi/reggie/common/Constant.java
+++ src/main/java/com/shi/reggie/common/Constant.java
@@ -4,5 +4,7 @@ import java.io.File;
 
 public class Constant {
 
-    public static final String BASE_PATH="F:"+ File.separator+"ideaProjects"+File.separator+"reggie_take_out"+File.separator+"photos"+File.separator;
+//    public static final String BASE_PATH="F:"+ File.separator+"ideaProjects"+File.separator+"reggie_take_out"+File.separator+"photos"+File.separator;
+    //存放图片时的相对路径，此目录的父目录为项目，使用相对路径便于程序在不同平台的迁移。能够发挥java移植性强的优点
+    public static final String BASE_PATH="photos"+File.separator;
 }
