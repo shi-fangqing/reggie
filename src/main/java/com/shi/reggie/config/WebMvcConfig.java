085c654e3ec1732c4ae71d708ec29c9c69462758
wuyu <wuyu@git.com>
2023-04-25 17:01:32 +0800

    添加swagger配置

--- src/main/java/com/shi/reggie/config/WebMvcConfig.java
+++ src/main/java/com/shi/reggie/config/WebMvcConfig.java
@@ -3,21 +3,31 @@ package com.shi.reggie.config;
 
 import ch.qos.logback.classic.pattern.MessageConverter;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.shi.reggie.common.JacksonObjectMapper;
 import com.shi.reggie.interceptor.LoginInterceptor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Slf4j
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     @Resource
@@ -32,6 +42,9 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("开启静态资源映射....");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     /**
@@ -62,6 +75,25 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
                         "/front/**",
                         "/user/login",
                         "/user/logout",
-                        "/user/sendMsg");
+                        "/user/sendMsg",
+                        "/doc.html",
+                        "/webjars/**",
+                        "/swagger-resources",
+                        "/v2/api-docs");
+    }
+
+    /**
+     * Swagger接口管理
+     * @return
+     */
+    @Bean
+    public Docket createRestApi(){
+        //文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder().title("瑞吉外卖").version("1.0").description("瑞吉外卖接口文档").build())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.shi.reggie.controller"))
+                .paths(PathSelectors.any())
+                .build();
     }
 }
